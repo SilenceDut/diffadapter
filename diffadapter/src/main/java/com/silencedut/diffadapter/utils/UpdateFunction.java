35f6c7973fc73af17968c924eff019df052972c9
SilenceDut <silencedutchman@foxmail.com>
2018-12-24 13:04:24 +0800

    compileOnly for recyclerview

--- diffadapter/src/main/java/com/silencedut/diffadapter/utils/UpdateFunction.java
+++ diffadapter/src/main/java/com/silencedut/diffadapter/utils/UpdateFunction.java
@@ -22,7 +22,7 @@ public interface UpdateFunction<I,R extends BaseMutableData> {
     Object providerMatchFeature(@NonNull I input);
 
     /**
-     * 只需关心自己需要更改的部分
+     * 匹配到对应的数据，如果符合条件的数据有很多个，可能会被回调多次
      * @param input 是数据改变的部分数据源
      * @param originalData 需要改变的数据项
      * @return 改变后的数据项
